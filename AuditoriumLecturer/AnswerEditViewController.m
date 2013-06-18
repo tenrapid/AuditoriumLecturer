5450905a8741689fb64d8f2f1f4fe9efddc14d90
tenrapid <matthiasrahne@gmail.com>
2013-06-19 00:42:49 +0200

    autogrowing textfield in AnswerEditView

--- AuditoriumLecturer/AnswerEditViewController.m
+++ AuditoriumLecturer/AnswerEditViewController.m
@@ -13,16 +13,44 @@
 
 NSString * const AnswerEditViewHeightDidChangeNotification = @"AnswerEditViewHeightDidChangeNotification";
 
+@interface AnswerEditViewController ()
+
+@property (assign) IBOutlet NSTextField *answerTextField;
+@property (assign) IBOutlet NSTextField *feedbackTextField;
+
+@end
+
 @implementation AnswerEditViewController
 
 @synthesize plusButton;
 @synthesize minusButton;
+@synthesize answerTextField;
+@synthesize feedbackTextField;
 
 - (AnswerEditViewController *)initWithAnswer:(Answer *)answer;
 {
     self = [super initWithNibName:@"AnswerEdit" bundle:nil];
     if (self) {
         self.representedObject = answer;
+		[self view];
+		
+		NSRect frame, bounds;
+		float height;
+		
+		frame = [answerTextField frame];
+		bounds = frame;
+		bounds.size.height = CGFLOAT_MAX;
+		height = [answerTextField.cell cellSizeForBounds: bounds].height;
+		frame.size.height = height;
+		[answerTextField setFrame:frame];
+		
+		frame = [feedbackTextField frame];
+		bounds = frame;
+		bounds.size.height = CGFLOAT_MAX;
+		height = [feedbackTextField.cell cellSizeForBounds: bounds].height;
+		frame.size.height = height;
+		[feedbackTextField setFrame:frame];
+
 		[self updateViewHeight];
     }
     return self;
@@ -37,10 +65,30 @@ NSString * const AnswerEditViewHeightDidChangeNotification = @"AnswerEditViewHei
 {
 	Answer *answer = self.representedObject;
 	Question *question = answer.question;
+	
+	float height = 0;
 
+	if (question.type == QuestionSingleChoiceType) {
+		if ([feedbackTextField isHidden]) {
+			[feedbackTextField setHidden:NO];
+		}
+		NSRect feedbackFrame = feedbackTextField.frame;
+		height += feedbackFrame.size.height + 5;
+		feedbackFrame.origin.y = 0;
+		[feedbackTextField setFrame:feedbackFrame];
+	}
+	else {
+		if (![feedbackTextField isHidden]) {
+			[feedbackTextField setHidden:YES];
+		}
+	}
+	NSRect answerFrame = answerTextField.frame;
+	answerFrame.origin.y = height;
+	[answerTextField setFrame:answerFrame];
+	height += answerFrame.size.height;
+	
 	NSRect frame = [self.view frame];
-//	frame.origin.y += frame.size.height - height;
-	frame.size.height = question.type == QuestionSingleChoiceType ? 49 : 22;
+	frame.size.height = height;
 	[self.view setFrame:frame];
 }
 
@@ -57,6 +105,18 @@ NSString * const AnswerEditViewHeightDidChangeNotification = @"AnswerEditViewHei
 	Answer *answer = self.representedObject;
 	[answer.question removeAnswersObject:answer];
 	[answer.managedObjectContext deleteObject:answer];
+}
+
+- (void)controlTextDidChange:(NSNotification *)notification
+{
+	NSTextField *textField = notification.object;
+	NSTextView *textView = [notification.userInfo objectForKey:@"NSFieldEditor"];
+	NSRect textFieldFrame = textField.frame;
+	NSRect textViewFrame = [textView.textContainer.layoutManager usedRectForTextContainer:textView.textContainer];
+	textFieldFrame.size.height = textViewFrame.size.height + 5;
+	[textField setFrame:textFieldFrame];
+	[self updateViewHeight];
+	[[NSNotificationCenter defaultCenter] postNotificationName:AnswerEditViewHeightDidChangeNotification object:self];
 }
 
 @end
